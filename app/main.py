d2ff772fd584974b3f834e02ffd144a14ce71309
Ваше Имя <you@example.com>
2025-12-29 16:44:57 +0300

    переделываю Аутентификация 1

--- app/main.py
+++ app/main.py
@@ -3,7 +3,6 @@ from fastapi.responses import RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.cors import CORSMiddleware
 from loguru import logger
-from app.middleware.token_validation import TokenValidationMiddleware
 from app.api import auth, web_cookie
 import os
 
@@ -13,18 +12,6 @@ app = FastAPI(title="SystemDM API")
 current_dir = os.path.dirname(os.path.abspath(__file__))
 static_path = os.path.join(current_dir, "static")
 app.mount("/static", StaticFiles(directory=static_path), name="static")
-
-# # CORS middleware
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=["*"],
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
-
-# # Token validation middleware
-# app.add_middleware(TokenValidationMiddleware)
 
 # Регистрируем роутеры
 app.include_router(auth.router, prefix="/api/auth", tags=["auth"])
